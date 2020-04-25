7e3877e46e5f3c526bc13d87bbd69404df48c89f
ddetone <ddetone@magicleap.com>
2020-04-25 16:26:34 -0700

    updates to p10k

--- update.sh
+++ update.sh
@@ -8,5 +8,5 @@ ln -sf .dotfiles/.tmux.conf .
 ln -sf .dotfiles/.vimrc .
 ln -sf .dotfiles/.vim .
 # my powershell10k config file
-ln -sf .dotfiles/p10k.zsh .
+ln -sf .dotfiles/.p10k.zsh .
 #ln -sf .dotfiles/ddetone.zsh-theme .oh-my-zsh/custom/themes/
